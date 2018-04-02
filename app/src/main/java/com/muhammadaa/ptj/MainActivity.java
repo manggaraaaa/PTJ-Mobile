857e49f60d6b536187a8ca9050b0a45a427a660e
muhammadaa <muh.ari.anggara@gmail.com>
2018-04-02 23:44:30 +0700

    add current time date

--- app/src/main/java/com/muhammadaa/ptj/MainActivity.java
+++ app/src/main/java/com/muhammadaa/ptj/MainActivity.java
@@ -1,5 +1,9 @@
 package com.muhammadaa.ptj;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -10,6 +14,10 @@ import android.widget.TextView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import butterknife.BindAnim;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.navigation)
     BottomNavigationViewEx navigation;
+    @BindView(R.id.date)
+    TextView mDate;
+    @BindView(R.id.time)
+    TextView mTime;
+
+    private BroadcastReceiver _broadcastReceiver;
+    private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("HH:mm");
+    private final SimpleDateFormat _sdfWatchDate = new SimpleDateFormat("dd MMMM yyyy");
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -46,9 +62,35 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        // initialisasi Date & Time
+        Date currentTime = Calendar.getInstance().getTime();
+        mDate.setText(_sdfWatchDate.format(currentTime));
+        mTime.setText(_sdfWatchTime.format(currentTime));
+
         navigation.enableAnimation(false);
         navigation.enableShiftingMode(false);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        _broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context ctx, Intent intent) {
+                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                    mTime.setText(_sdfWatchTime.format(new Date()));
+            }
+        };
+
+        registerReceiver(_broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (_broadcastReceiver != null)
+            unregisterReceiver(_broadcastReceiver);
     }
 
 }
