6279a5ee4027226fd7a5b1c73be6911e45e9f1fd
muhammadaa <muh.ari.anggara@gmail.com>
2018-04-04 23:03:17 +0700

    init slider image

--- app/src/main/java/com/muhammadaa/ptj/views/HomeFragment.java
+++ app/src/main/java/com/muhammadaa/ptj/views/HomeFragment.java
@@ -10,12 +10,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.muhammadaa.ptj.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +32,8 @@ public class HomeFragment extends Fragment {
     TextView mDate;
     @BindView(R.id.time)
     TextView mTime;
+    @BindView(R.id.slider)
+    SliderLayout mSlider;
 
     private BroadcastReceiver _broadcastReceiver;
     private final SimpleDateFormat _sdfWatchTime = new SimpleDateFormat("HH:mm");
@@ -48,7 +56,51 @@ public class HomeFragment extends Fragment {
         mDate.setText(_sdfWatchDate.format(currentTime));
         mTime.setText(_sdfWatchTime.format(currentTime));
 
+        // resource slider Image
+//        HashMap<String, String> url_maps = new HashMap<String, String>();
+//        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+//        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+//        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+//        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("Proses memberi media Polibag pada tanaman",R.drawable.ptj_4);
+        file_maps.put("Bahan untuk media tanaman",R.drawable.ptj_3);
+        file_maps.put("Bibit Durian",R.drawable.ptj_2);
+        file_maps.put("Proses Stek tanaman", R.drawable.ptj_1);
+
+        initSlider(file_maps);
+
         return v;
+    }
+
+    private void initSlider(HashMap<String, Integer> url_maps) {
+        for (String name : url_maps.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(getContext());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(new BaseSliderView.OnSliderClickListener() {
+                        @Override
+                        public void onSliderClick(BaseSliderView slider) {
+                            Toast.makeText(getContext(), slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra", name);
+
+            mSlider.addSlider(textSliderView);
+        }
+
+        mSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Top);
+        mSlider.setCustomAnimation(new DescriptionAnimation());
+        mSlider.setDuration(4000);
     }
 
     @Override
@@ -67,6 +119,7 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onStop() {
+        mSlider.stopAutoCycle();
         super.onStop();
         if (_broadcastReceiver != null)
             getActivity().unregisterReceiver(_broadcastReceiver);
